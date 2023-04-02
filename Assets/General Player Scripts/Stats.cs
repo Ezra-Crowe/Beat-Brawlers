e0af670bd28a9ed8a3ce61e060a2aec55954ed67
Ezra Crowe <ecrowe@oberlin.edu>
2023-04-02 13:50:27 -0400

    added health stat

---
+++ Assets/General Player Scripts/Stats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public double health = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
